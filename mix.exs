ef03cda47853a33eeb95d8bc049310957755f1c0
Shane Logsdon <shane@shanelogsdon.com>
2014-12-03 16:00:49 -0500

    adding bits for hex

--- mix.exs
+++ mix.exs
@@ -5,25 +5,36 @@ defmodule Mandrillex.Mixfile do
     [ app: :mandrillex,
       version: "0.2.0",
       elixir: "~> 1.0",
+      description: description,
+      package: package,
       deps: deps ]
   end
 
-  # Configuration for the OTP application
   def application do
     [ mod: { Mandrillex, [] },
       applications: [:httpoison] ]
   end
 
-  # Returns the list of dependencies in the format:
-  # { :foobar, git: "https://github.com/elixir-lang/foobar.git", tag: "0.1" }
-  #
-  # To specify particular versions, regardless of the tag, do:
-  # { :barbat, "~> 0.1", github: "elixir-lang/barbat" }
   defp deps do
     [
       { :httpoison, "~> 0.3" },
       { :hackney, "~> 0.13.1" }, # not included in hex version of httpoison :(
       { :jsex, "~> 2.0.0" }
     ]
+  end
+
+  defp description do
+    """
+    A Mandrill wrapper for Elixir
+
+    Requires an active account with Mandrill (http://mandrill.com).
+    """
+  end
+
+  defp package do
+    [ files: [ "lib", "mix.exs", "README.md", "LICENSE" ],
+      contributors: [ "Shane Logsdon" ],
+      licenses: [ "MIT" ],
+      links: %{ "GitHub" => "https://github.com/slogsdon/mandrillex" } ]
   end
 end
