d27ae19f9b3f2afffba4cc5e9822b4810badf776
Justin Schneck <jschneck@mac.com>
2015-03-23 17:09:15 -0400

    Updated to latest HTTPoison. Changed post to post!

--- mix.exs
+++ mix.exs
@@ -17,8 +17,7 @@ defmodule Mandrill.Mixfile do
 
   defp deps do
     [
-      { :httpoison, "~> 0.3" },
-      { :hackney, "~> 0.13.1" }, # not included in hex version of httpoison :(
+      { :httpoison, "~> 0.6" },
       { :exjsx, "~> 3.1.0", app: false },
       { :ex_doc, "~> 0.6.1", only: :docs },
       { :earmark, "~> 0.1.12", only: :docs }
