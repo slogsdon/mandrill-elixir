2ccef6118c9fb1cc83a1e00a00414e844afc17f6
Shane Logsdon <shane@shanelogsdon.com>
2015-07-13 22:53:41 -0400

    v0.4.1

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Mandrill.Mixfile do
 
   def project do
     [ app: :mandrill,
-      version: "0.4.0",
+      version: "0.4.1",
       elixir: "~> 1.0",
       description: description,
       package: package,
