5087317355c0057de2c14672ac7f5a2094b40ab5
Nick Kezhaya <nick@whitepaperclip.com>
2019-11-19 20:11:09 -0600

    Set minimum Elixir version to 1.4

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Mandrill.Mixfile do
     [
       app: :mandrill,
       version: "0.5.0",
-      elixir: "~> 1.0",
+      elixir: "~> 1.4",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
       description: description(),
