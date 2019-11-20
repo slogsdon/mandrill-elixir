2581f326749f7dc4d6b737698f1517f91fee071d
Nick Kezhaya <nick@whitepaperclip.com>
2019-11-19 20:05:13 -0600

    Remove unnecessary test

--- test/mandrill_test.exs
+++ test/mandrill_test.exs
@@ -8,11 +8,4 @@ defmodule MandrillTest do
   test "ping returns pong" do
     assert Mandrill.Users.ping() == "PONG!"
   end
-
-  test "process_response_body returns a map with string keys" do
-    assert Mandrill.process_response_body("{\"language\": \"elixir\", \"awesome\": true}") == %{
-             "awesome" => true,
-             "language" => "elixir"
-           }
-  end
 end
